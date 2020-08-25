5d524c4bda75c706766f3610dd3696aab2a03d53
Alex <garvatskiyaleksey@gmail.com>
2020-08-25 16:49:30 +0300

    Conditional Statements

--- js/script.js
+++ js/script.js
@@ -7,3 +7,14 @@ answers[1] = prompt('как ваша фамилия ? ', '');
 answers[2] = prompt('сколько вам лет ? ', '');
 
 console.log(typeof(answers));
+
+// условные операторы 
+const user = +prompt('Сколько вам лет? ', '');
+
+if (user >= 18) {
+    console.log('вы совершенно летний');
+} else if (user <= 18) {
+     console.log('вам нет 18');
+} else {
+    console.log('error');
+}
