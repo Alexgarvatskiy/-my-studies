4ff6fa2c56b26721aab9869ed20e355165c396f6
Alex <garvatskiyaleksey@gmail.com>
2020-08-25 14:49:35 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = []; 
+
+answers[0] = prompt('как ваше имя ? ', '');
+answers[1] = prompt('как ваша фамилия ? ', '');
+answers[2] = prompt('сколько вам лет ? ', '');
+
+console.log(typeof(answers));
